2c9d5679b8aafb9f3591bb6bf02f171d5d68585d
daoqi <hs17629792976@163.com>
2024-12-29 18:23:46 +0800

    1

---
+++ 杂项/pbds.cpp
@@ -0,0 +1,6 @@
+
+#include <bits/extc++.h>
+
+using namespace __gnu_pbds;
+
+using Tree = tree<std::pair<i64, i64>, null_type, std::less<std::pair<i64, i64>>, rb_tree_tag, tree_order_statistics_node_update>;
