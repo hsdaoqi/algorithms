ea4e2c34b016976210eb366b328565254f278309
daoqi <hs17629792976@163.com>
2025-01-08 18:35:39 +0800

    update矩阵快速幂

--- 数学/矩阵快速幂.cpp
+++ 数学/矩阵快速幂.cpp
@@ -1,35 +1,49 @@
-template<class T>
-struct grid {
-    int n;
-    std::vector<std::vector<T>> c;
+/**
+ * 矩阵（Matrix）
+**/
+using i64 = long long;
+constexpr int N = 2;
+constexpr int mod = 1e9 + 7;
+using Matrix = std::array<i64, N * N>;
 
-    grid(int n_ = 100) : n(n_) {
-        c.resize(n + 1, std::vector<T>(n + 1, {}));
-    }
-};
+int id(int i, int j) {
+    return i * N + j;
+}
 
-template<class T>
-grid<T> operator*(grid<T> &x, grid<T> &y) {
-    int n = x.n;
-    grid<T> tmp(n);
-    for (int i = 1; i <= n; i++) {
-        for (int j = 1; j <= n; j++) {
-            for (int k = 1; k <= n; k++) {
-                tmp.c[i][j] = (tmp.c[i][j] + x.c[i][k] * y.c[k][j]) % mod;
+Matrix operator*(const Matrix &a, const Matrix &b) {
+    Matrix c{};
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            for (int k = 0; k < N; k++) {
+                c[id(i, j)] = (c[id(i, j)] + a[id(i, k)] * b[id(k, j)]) % mod;
             }
         }
     }
-    return tmp;
+    return c;
 }
 
-template<class T>
-grid<T> power(grid<T> a, T b) {
-    grid<T> ans(a.n);
-    for (int i = 1; i <= ans.n; i++) ans.c[i][i] = 1;
-    while (b) {
+Matrix operator+(const Matrix &a, const Matrix &b) {
+    Matrix c{};
+    for (int i = 0; i < N * N; i++) {
+        c[i] = (a[i] + b[i]) % mod;
+    }
+    return c;
+}
+
+Matrix power(Matrix a, i64 b) {
+    Matrix ans{};
+    for (int i = 0; i < N; i++) ans[id(i, i)] = 1;
+    for (; b; b >>= 1, a = a * a) {
         if (b & 1) ans = ans * a;
-        a = a * a;
-        b >>= 1;
     }
     return ans;
+}
+
+Matrix init(Matrix &mat, int x = 0) {
+    if (x == 0) {
+        mat.fill(0);
+    } else {
+        for (int i = 0; i < N; i++) mat[id(i, i)] = x;
+    }
+    return mat;
 }
