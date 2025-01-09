fa52ba704f433d750fa379a047f47c5512b4f336
daoqi <hs17629792976@163.com>
2025-01-09 21:48:02 +0800

    1

--- 数学/矩阵快速幂.cpp
+++ 数学/矩阵快速幂.cpp
@@ -2,19 +2,30 @@
  * 矩阵（Matrix）
 **/
 using i64 = long long;
-constexpr int N = 2;
+
+constexpr int N = 100;
 constexpr int mod = 1e9 + 7;
+int M = N;
 using Matrix = std::array<i64, N * N>;
 
 int id(int i, int j) {
-    return i * N + j;
+    return i * M + j;
+}
+
+Matrix init(Matrix &mat, int x = 0) {
+    if (x == 0) {
+        mat.fill(0);
+    } else {
+        for (int i = 0; i < M; i++) mat[id(i, i)] = x;
+    }
+    return mat;
 }
 
 Matrix operator*(const Matrix &a, const Matrix &b) {
     Matrix c{};
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < N; j++) {
-            for (int k = 0; k < N; k++) {
+    for (int i = 0; i < M; i++) {
+        for (int j = 0; j < M; j++) {
+            for (int k = 0; k < M; k++) {
                 c[id(i, j)] = (c[id(i, j)] + a[id(i, k)] * b[id(k, j)]) % mod;
             }
         }
@@ -24,7 +35,7 @@ Matrix operator*(const Matrix &a, const Matrix &b) {
 
 Matrix operator+(const Matrix &a, const Matrix &b) {
     Matrix c{};
-    for (int i = 0; i < N * N; i++) {
+    for (int i = 0; i < M * M; i++) {
         c[i] = (a[i] + b[i]) % mod;
     }
     return c;
@@ -32,18 +43,9 @@ Matrix operator+(const Matrix &a, const Matrix &b) {
 
 Matrix power(Matrix a, i64 b) {
     Matrix ans{};
-    for (int i = 0; i < N; i++) ans[id(i, i)] = 1;
+    init(ans, 1);
     for (; b; b >>= 1, a = a * a) {
         if (b & 1) ans = ans * a;
     }
     return ans;
-}
-
-Matrix init(Matrix &mat, int x = 0) {
-    if (x == 0) {
-        mat.fill(0);
-    } else {
-        for (int i = 0; i < N; i++) mat[id(i, i)] = x;
-    }
-    return mat;
 }
