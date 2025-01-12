1fcb8bf5c99fdeb611357b3025a07a86970362fb
daoqi <hs17629792976@163.com>
2025-01-12 20:22:04 +0800

    1

--- 数据结构/可撤销并查集.cpp
+++ 数据结构/可撤销并查集.cpp
@@ -1,22 +1,28 @@
 /**   可撤销并查集（DSU With Rollback）
- *    2024-09-17: https://qoj.ac/submission/569639
 **/
 struct DSU {
-    std::vector<int> siz;
-    std::vector<int> f;
+    int n;
+    std::vector<int> siz, f;
     std::vector<std::array<int, 2>> his;
-    
-    DSU(int n) : siz(n + 1, 1), f(n + 1) {
+
+    DSU(int _n = 1) {
+        init(_n);
+    }
+
+    void init(int _n) {
+        this->n = _n;
+        siz.assign(n, 1);
+        f.resize(n);
         std::iota(f.begin(), f.end(), 0);
     }
-    
+
     int find(int x) {
         while (f[x] != x) {
             x = f[x];
         }
         return x;
     }
-    
+
     bool merge(int x, int y) {
         x = find(x);
         y = find(y);
@@ -31,11 +37,11 @@ struct DSU {
         f[y] = x;
         return true;
     }
-    
+
     int time() {
         return his.size();
     }
-    
+
     void revert(int tm) {
         while (his.size() > tm) {
             auto [x, y] = his.back();
@@ -43,5 +49,13 @@ struct DSU {
             f[y] = y;
             siz[x] -= siz[y];
         }
+    }
+
+    int size(int p) {
+        return siz[find(p)];
+    }
+
+    bool same(int u, int v) {
+        return find(u) == find(v);
     }
 };
